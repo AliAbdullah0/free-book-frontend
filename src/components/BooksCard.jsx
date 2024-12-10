8c6607d3b67c40b310d89975421cf6bbdfa3139d
AliAbdullah0 <aliabdullah10nov2006@gmail.com>
2024-12-10 19:32:29 +0500

    changed dowload url

--- src/components/BooksCard.jsx
+++ src/components/BooksCard.jsx
@@ -61,8 +61,9 @@ function BooksCard({ path }) {
                             </p>
                             {book.PDFfile && (
                                 <a
-                                  href={`${import.meta.env.VITE_FILE_URL}${book.PDFfile.url}`}
+                                  href={`${book.PDFfile.url}`}
                                   download
+                                  contentType = 'application/pdf'
                                   target="_blank"
                                   rel="noopener noreferrer"
                                   className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
