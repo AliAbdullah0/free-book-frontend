3b6d0e387f5e9a98c8c9fba5971ab7bd5a36594f
AliAbdullah0 <aliabdullah10nov2006@gmail.com>
2024-12-10 20:47:09 +0500

    added changes

--- src/components/BooksCard.jsx
+++ src/components/BooksCard.jsx
@@ -87,15 +87,7 @@ function BooksCard({ path }) {
                                             />
                                         </svg>
                                     </a>
-                                    <Link
-                                        to={{
-                                            pathname: "/pdf-viewer",
-                                            search: `?url=${encodeURIComponent(book.PDFfile.url)}`
-                                        }}
-                                        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-                                    >
-                                        View PDF
-                                    </Link>
+                        
                                 </div>
                             )}
                         </div>
